6636dbb04620ee3f1f2b770fbbeeea6b018c627d
DanelRisa <yerkuliyeva@gmail.com>
2023-08-06 14:53:45 +0600

    plswork2

--- app/jobs/jobs.py
+++ app/jobs/jobs.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from .tasks import parse_both_websites
+from .tasks import scrape_arbuz, scrape_galmart, parse_both_websites
 
+# scheduler = BackgroundScheduler()
 
 # scheduler.add_job(scrape, "cron", hour=21, minute=30)
 # # scheduler.add_job(scrape_news, "interval", hours=1)
@@ -8,7 +9,8 @@ from .tasks import parse_both_websites
 
 scheduler = BackgroundScheduler()
 
-scheduler.add_job(parse_both_websites, "cron", hour=14, minute=40)
+scheduler.add_job(parse_both_websites, "cron", hour=14, minute=55)
+# scheduler.add_job(scrape_galmart, "cron", hour=00, minute=55)
 
-# scheduler.start()
+scheduler.start()
 
