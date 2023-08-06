afdff410215ccd7652efe969e9b015dbfb2d486b
DanelRisa <yerkuliyeva@gmail.com>
2023-08-06 13:39:26 +0600

    plswork

--- app/jobs/jobs.py
+++ app/jobs/jobs.py
@@ -1,7 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from .tasks import scrape_arbuz, scrape_galmart, parse_both_websites
+from .tasks import parse_both_websites
 
-# scheduler = BackgroundScheduler()
 
 # scheduler.add_job(scrape, "cron", hour=21, minute=30)
 # # scheduler.add_job(scrape_news, "interval", hours=1)
@@ -9,16 +8,7 @@ from .tasks import scrape_arbuz, scrape_galmart, parse_both_websites
 
 scheduler = BackgroundScheduler()
 
-scheduler.add_job(parse_both_websites, "cron", hour=2, minute=40)
-# scheduler.add_job(scrape_galmart, "cron", hour=00, minute=55)
+scheduler.add_job(parse_both_websites, "cron", hour=13, minute=45)
 
 # scheduler.start()
 
-# try:
-#     while True:
-#         pass
-# except (KeyboardInterrupt, SystemExit):
-#     scheduler.shutdown()
-#     client.close()
-
-# print('Data removed and updated in MongoDB.')
